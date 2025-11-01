19d218aae6d44f993a995bb222973375018ab639
Emre Sarıkaynak <emresarikaynak@aof.anadolu.edu.tr>
2025-11-01 17:04:30 +0300

    fix: next.config.js static export kaldırıldı

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Cloudflare Pages için static export
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
+  // Cloudflare Pages için optimizasyon
   images: {
     unoptimized: true
   }
