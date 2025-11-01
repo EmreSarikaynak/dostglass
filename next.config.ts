7027ecae7f20ed024af4e5ed80ef3ee190a063f1
Emre Sarıkaynak <emresarikaynak@aof.anadolu.edu.tr>
2025-11-01 17:46:42 +0300

    fix: Autocomplete type casting hataları düzeltildi 32 3  + strict:false

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // Cloudflare için gerekli
     remotePatterns: [
       {
         protocol: 'https',
